d6e95d07fe98d920825008e6287d5421262ee4ec
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-12 21:21:56 +0200

    update cosmo.swift

--- cosmo.swift
+++ cosmo.swift
@@ -1,9 +1,48 @@
+import Foundation
+
 class cosmo {
 
-    func cosmo_greet () {
-        print("Hello cosmo")
+    func readFileName () -> String {
+
+        let filePath = readLine()
+
+        return filePath!
+
     }
+
+    func readDataFromFile (_ path: String) -> String {
+        
+        var data = ""
+
+        if freopen(path, "r", stdin) == nil {
+            perror(path)
+        }
+
+        while let dataFluxStream = readLine () {
+            data += dataFluxStream
+        }
+
+        return data; 
+    }
+
+    func putsInFile (_ filename: String, message: String) -> Void {
+
+        do {
+            try message.write(toFile: filename, atomically: false, encoding: String.Encoding.utf8)
+
+        } catch let error as NSError {
+    
+            print("Unable to process: \(error)")
+    
+        } catch {
+
+        }
+    }
+
 }
 
-let greet_cosmo = cosmo()
-greet_cosmo.cosmo_greet()
+let cosmoObjectRefference = cosmo()
+let data = cosmoObjectRefference.readDataFromFile(cosmoObjectRefference.readFileName())
+cosmoObjectRefference.putsInFile("D:\\code\\workSpace\\cosmo\\ouputDataStream.dataFlux", message: data)
+
+// D:\code\workSpace\cosmo\DataStream.dataFlux
